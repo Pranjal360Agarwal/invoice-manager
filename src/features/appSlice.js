9c410df12b3246062bba420d67e95f239525f6fa
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-11-19 21:46:22 +0530

    Add the appslice feature

---
+++ src/features/appSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const appSlice = createSlice({
+    name: 'app',
+    initialState: {
+        invoices: [],
+        products: [],
+        customers: [],
+    },
+    reducers: {
+        setInvoices(state, action) {
+            state.invoices = action.payload;
+        },
+        setProducts(state, action) {
+            state.products = action.payload;
+        },
+        setCustomers(state, action) {
+            state.customers = action.payload;
+        },
+    },
+});
+
+export const { setInvoices, setProducts, setCustomers } = appSlice.actions;
+export default appSlice.reducer;
